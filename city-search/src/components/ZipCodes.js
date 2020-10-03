24aba14f5260c2d4379b603a675ac1710e26fd4d
Sett Hein <settnaing199@gmail.com>
2020-10-03 13:01:00 -0400

    add new citySearch

---
+++ city-search/src/components/ZipCodes.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ZipCodes(props){
+    return(
+      <div>
+        <ul>
+          <li>{props.zip}</li>
+        </ul>
+        
+      </div>
+    )
+  }
+
+export default ZipCodes
